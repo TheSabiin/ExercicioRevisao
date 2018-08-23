eacc9e530b3cd7a9432997df74db2e34bfc99f3a
opet <opet@Academico.edu>
2018-08-23 11:17:47 -0300

    Projeto Fruta

---
+++ Frutas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace frutas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o nome da fruta: ");
+            var fruta = Console.Readline();
+            Console.WriteLine();
+            Console.WriteLine("Quantas vezes gostaria de repetir: ");
+            var qtd = int.Parse(Console.ReadLine());
+
+            var saida = string.Empty;
+            for(int i = 0; i < qtd; i++)
+            {
+                if(saida.Lenght > 0)
+                {
+
+                    saida += " * ";
+                    saida += fruta;
+                }
+                Console.WriteLine(saida);
+            }
+
+        }
+    }
+}
